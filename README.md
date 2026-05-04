40270e47a7c514677a1a332f184e22871a02c123
Anish Kondamadugula <anish.kondamadugula@gmail.com>
2026-05-03 20:25:41 -0700

    Complete README answers and Jest tests

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Make sure you make a PR to your own repo's main and not the class' repo!! Otherw
 
 
 My name: Anish Kondamadugula
+
+## 1. 
+I wouldn't just use a unit test to test the "message" feature of a messaging application, because semnding a message is dependent on various parts working together, like the UI, input handling, server/database, and message display. A unit test could be helpful for testing a helper function corressponding to messages, but the full send-message feature must be tested with an integration or end-to-end test.
+
+## 2.
+I would use a unit test to test the "max message length" feature of a messaging application, because if the user shouldn't be able to type over 80 characters, this can be tested as a small isolated piece of logic. I would test that messages with 80 or less characters are allowed and those that are over 80 characters are rejected or prevented.
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,46 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const faceImage = document.querySelector('#explore > img');
+  const textArea = document.getElementById('text-to-speak');
+  const voiceSelect = document.getElementById('voice-select');
+  const talkButton = document.querySelector('button');
+
+  let voices = [];
+
+  function loadVoices() {
+    voices = speechSynthesis.getVoices();
+
+    voiceSelect.innerHTML = '<option value="select" disabled selected>Select Voice:</option>';
+
+    voices.forEach((voice, index) => {
+      const option = document.createElement('option');
+      option.value = index;
+      option.textContent = `${voice.name} (${voice.lang})`;
+      voiceSelect.appendChild(option);
+    });
+  }
+
+  loadVoices();
+  speechSynthesis.addEventListener('voiceschanged', loadVoices);
+
+  talkButton.addEventListener('click', () => {
+    const utterance = new SpeechSynthesisUtterance(textArea.value);
+
+    if (voiceSelect.value !== 'select') {
+      utterance.voice = voices[voiceSelect.value];
+    }
+
+    utterance.addEventListener('start', () => {
+      faceImage.src = 'assets/images/smiling-open.png';
+      faceImage.alt = 'Open smiling face';
+    });
+
+    utterance.addEventListener('end', () => {
+      faceImage.src = 'assets/images/smiling.png';
+      faceImage.alt = 'Smiling face';
+    });
+
+    speechSynthesis.speak(utterance);
+  });
 }
